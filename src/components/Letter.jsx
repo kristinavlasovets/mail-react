e446e859747070825b9aa9835161a2d87d4607d4
kristinavlasovets <61822704+kristinavlasovets@users.noreply.github.com>
2022-11-28 13:32:31 +0300

    changed the main page

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,6 +1,59 @@
-import React from 'react';
-import {Box} from '@mui/material';
+import React, {useState, useEffect} from 'react';
+import {
+	Box,
+	List,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	Collapse,
+} from '@mui/material';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import axios from 'axios';
 
-export const Letter = () => {
-	return <Box></Box>;
+export const Letter = ({letters}) => {
+	const [user, setUser] = useState(null);
+	const [open, setOpen] = React.useState(false);
+
+	console.log(letters)
+
+	const handleClick = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<Box>
+			<List
+				sx={{
+					m: '10px auto',
+					width: '100%',
+					maxWidth: '80vw',
+					border: '1px solid lightgray',
+					borderRadius: '10px',
+				}}
+				component="nav"
+			>
+				<ListItemButton onClick={handleClick}>
+					<ListItemIcon>
+						<DraftsIcon />
+					</ListItemIcon>
+						<ListItemText>{letters[0].subject}</ListItemText>
+					{open ? <ExpandLess /> : <ExpandMore />}
+				</ListItemButton>
+				<ListItemButton>
+					<ListItemText>From:{letters[0].sender} </ListItemText>
+					<ListItemText>Sent: {letters[0].date}</ListItemText>
+				</ListItemButton>
+
+				<Collapse in={open} timeout="auto" unmountOnExit>
+					<List component="div" disablePadding>
+						<ListItemButton sx={{pl: 4}}>
+								<ListItemText>{letters[0].text}</ListItemText>
+						</ListItemButton>
+					</List>
+				</Collapse>
+			</List>
+		</Box>
+	)
 };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,40 +1,40 @@
-import React, {useState} from 'react';
-import {enterCall} from '../apiCalls';
-import {Box, TextField, Button} from '@mui/material';
-import {useEnterContext} from '../context/enterContext';
+import React, { useState } from "react";
+import { enterCall } from "../apiCalls";
+import { Box, TextField, Button } from "@mui/material";
+import { useEnterContext } from "../context/enterContext";
 
 export const LoginPage = () => {
-	const [name, setName] = useState('');
-	const {dispatch} = useEnterContext();
-	const handleEnter = (e) => {
-		e.preventDefault();
-		enterCall({username: name}, dispatch);
-	};
-	return (
-		<Box
-			sx={{
-				m: '20vh auto',
-				width: 500,
-				display: 'flex',
-				flexDirection: 'column',
-			}}
-			component="form"
-			onSubmit={handleEnter}
-		>
-			<TextField
-				sx={{m: '0 auto', width: 200}}
-				onChange={(event) => setName(event.target.value)}
-				label="Your name"
-				variant="outlined"
-				value={name}
-			/>
-			<Button
-				sx={{m: '20px auto', width: 100}}
-				type="submit"
-				variant="contained"
-			>
-				Enter
-			</Button>
-		</Box>
-	);
+  const [name, setName] = useState("");
+  const { dispatch } = useEnterContext();
+  const handleEnter = (e) => {
+    e.preventDefault();
+    enterCall({ username: name }, dispatch);
+  };
+  return (
+    <Box
+      sx={{
+        m: "20vh auto",
+        width: 500,
+        display: "flex",
+        flexDirection: "column",
+      }}
+      component="form"
+      onSubmit={handleEnter}
+    >
+      <TextField
+        sx={{ m: "0 auto", width: 200 }}
+        onChange={(event) => setName(event.target.value)}
+        label="Your name"
+        variant="outlined"
+        value={name}
+      />
+      <Button
+        sx={{ m: "20px auto", width: 100 }}
+        type="submit"
+        variant="contained"
+      >
+        Enter
+      </Button>
+    </Box>
+  );
 };
