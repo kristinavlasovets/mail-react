a2e5e55ee87346a09525d3846ef658733f62215f
Kristina Vlasovets <kristinavlasovets@gmail.com>
2022-11-29 15:09:02 +0300

    changed api call url

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const enterCall = async (username, dispatch) => {
 	try {
 		const res = await axios.post(
-			process.env.REACT_APP_SERVER_URL + '/enter/user',
+			'https://mail-nodejs-production.up.railway.app' + '/enter/user',
 			username
 		);
 		dispatch({type: 'ENTER_SUCCESS', payload: res.data});
